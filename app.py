742762795265909298439d07394ec3ff61ca654f
Chandrashekhar H S <chandrashekhar.hs.dev@gmail.com>
2026-06-14 15:30:15 +0000

    Fix Render dynamic port

--- app.py
+++ app.py
@@ -22,5 +22,8 @@ def weather(city):
         "temperature": "28C"
     })
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
